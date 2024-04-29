ae55ffc948bb0ad3ed52e13f8212443ad06342ab
Nojus <nojus.dziaugys@gmail.com>
2024-04-29 14:31:53 +0300

    finished my batis

--- src/main/java/org/example/lab1/usecases/PeopleMyBatis.java
+++ src/main/java/org/example/lab1/usecases/PeopleMyBatis.java
@@ -2,21 +2,21 @@ package org.example.lab1.usecases;
 
 import lombok.Getter;
 import lombok.Setter;
-import org.example.lab1.entities.Person;
+import org.example.lab1.mybatis.model.Person;
+import org.example.lab1.mybatis.dao.PersonMapper;
 import org.example.lab1.persistence.PeopleDAO;
-import javax.transaction.Transactional;
 
 import javax.annotation.PostConstruct;
 import javax.enterprise.inject.Model;
 import javax.inject.Inject;
+import javax.transaction.Transactional;
 import java.io.Serializable;
 import java.util.List;
-import java.util.logging.Logger;
 
 @Model
-public class People implements Serializable {
+public class PeopleMyBatis implements Serializable {
     @Inject
-    private PeopleDAO peopleDAO;
+    private PersonMapper personMapper;
 
     @Getter @Setter
     private Person personToCreate = new Person();
@@ -29,10 +29,10 @@ public class People implements Serializable {
 
     @Transactional
     public void createPerson(){
-        this.peopleDAO.persist(personToCreate);
+        this.personMapper.insert(personToCreate);
     }
 
     private void loadAllPeople(){
-        this.allPeople = peopleDAO.loadAll();
+        this.allPeople = personMapper.selectAll();
     }
 }
